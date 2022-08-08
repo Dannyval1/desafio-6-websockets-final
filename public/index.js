a9d3c8b79c34ebb140c6d97d37ea69d926a58b42
devalencia12 <devalencia@bancodeoccidente.com.co>
2022-08-08 09:56:41 -0500

    :sparkles: Entrega final

--- public/index.js
+++ public/index.js
@@ -1,21 +1,27 @@
 const socket = io();
 
-const inputChat = document.getElementById("chatting");
-const btnChat = document.getElementById("send");
-btnChat.addEventListener("click", () => {
-    socket.emit("mensaje", inputChat.value);
-    inputChat.value = "";
-})
+const tableProducts = document.getElementById("tableProducts");
 
 const inputEmail = document.getElementById("email");
 const btnEmail = document.getElementById("sendEmail");
 document.getElementById("sendEmail").addEventListener("click", () => {
-    socket.emit("correo", inputEmail.value);
+    //socket.emit("correo", inputEmail.value);
     inputChat.removeAttribute('disabled');
     btnChat.removeAttribute('disabled');
-    inputEmail.value = "";
     inputEmail.setAttribute('disabled', ''); 
     btnEmail.setAttribute('disabled', ''); 
+})
+
+const inputChat = document.getElementById("chatting");
+const btnChat = document.getElementById("send");
+btnChat.addEventListener("click", () => {
+    const message = {
+        mensaje: inputChat.value,
+        correo: inputEmail.value,
+        date: new Date()
+    }
+    socket.emit("mensaje", message);
+    inputChat.value = "";
 })
 
 const productName = document.getElementById("productName");
@@ -35,36 +41,55 @@ document.getElementById("save_product").addEventListener("click", () => {
 
 // Cliente
 socket.on("productos", (productos) => {
+    const conProducto = `
+    <thead>
+        <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Imagen</th>
+        </tr>
+    </thead>
+    `;
+    const sinProducto = `
+    <div class="alert alert-warning" role="alert">
+        No se encontraron productos
+    </div>
+    `;
+
+    const productoHTML = productos.map((prod, index) => {
+        return (`
+            <tbody>
+                <tr>
+                    <td>${prod.producto.nombre}</td>
+                    <td>${prod.producto.precio}</td>
+                    <td>
+                        <img src="${prod.producto.imagen}" class="img-prod" alt="${prod.producto.nombre}">
+                    </td>
+                </tr>
+            </tbody>
+        `)
+    }).join(" ");
+
     if(productos.length > 0){
-        const name = productos.map((producto) => `${producto.producto.nombre}`);
-        const precio = productos.map((producto) => `${producto.producto.precio}`);
-        const imagen = productos.map((producto) => `${producto.producto.imagen}`);
-      
-        let totalProducts = productos.length;
-        let nombreFinal;
-        let precioFinal;
-        let imagenFinal;
-        for(let i = 0; i <= totalProducts; i++){
-          nombreFinal += "<p>"+name+"</p>"
-          precioFinal += "<p>"+precio+"</p>"
-          imagenFinal += "<p>"+imagen+"</p>"
-        }
-        document.getElementById("name_product").innerHTML = nombreFinal;
-        document.getElementById("price_product").innerHTML = precioFinal;
-        document.getElementById("image_product").innerHTML = imagenFinal;
-    } else {
-        console.log("No hay productos")
+        tableProducts.innerHTML = conProducto + productoHTML;
+
+    }else{
+        tableProducts.innerHTML = sinProducto;
     }
-  
 });
 
 // Cliente
 socket.on('mensajes', (mensajes) => {
-    const mensajesInput = mensajes
-    .map(
-        (mensaje) => 
-        `SocketId: ${mensaje.socketid} -> Mensajes: ${mensaje.mensaje}`
-    )
-    .join("<br>")
-    document.getElementById("message").innerHTML = mensajesInput;
+    console.log("mensajes",mensajes);
+    const html = mensajes.map((elem, index) => {
+        return (
+            `<div>
+                <strong class='text-primary'>${elem.mensaje.correo}</strong>
+                <time class='text-muted'>${elem.mensaje.date}</time>
+                : <em>${elem.mensaje.mensaje}</em>
+            </div>`
+        )
+    }).join(" ");
+    const div = document.getElementById('message');
+    div.innerHTML = html;
 })
